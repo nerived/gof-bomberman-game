7325b629d2b907346b400a4fa063217452ab2e3b
eprikhodko <eprikhodkodev@gmail.com>
2024-02-16 21:45:31 +0100

    [gof-27] add start screen page

--- packages/client/src/pages/MainPage.tsx
+++ packages/client/src/pages/MainPage.tsx
@@ -1,5 +1,15 @@
 import { FC } from 'react'
+import { Button, PageContainer } from '../ui-kit'
+import { Logo } from '../ui-kit/Logo/Logo'
 
 export const MainPage: FC = () => {
-  return <div>MainPage</div>
+  return (
+    <PageContainer>
+      <Logo src="/logo.png" alt="bomberman logo" />
+
+      <div style={{ marginTop: '40px' }}>
+        <Button title="START" href="/registration" type="8bit" />
+      </div>
+    </PageContainer>
+  )
 }

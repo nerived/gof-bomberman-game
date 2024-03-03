f8ff04f2e8197dd407486dcd77d1d84fcf11e056
Mikhail <m.temartsev@gmail.com>
2024-03-03 21:05:40 +0300

    GOF-42 Добавить Service Workers

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -5,14 +5,13 @@ import ErrorBoundary from './shared/ErrorBoundary'
 
 function App() {
   useEffect(() => {
-    const fetchServerData = async () => {
-      const url = `http://localhost:${__SERVER_PORT__}`
-      const response = await fetch(url)
-      const data = await response.json()
-      console.log(data)
-    }
-
-    fetchServerData()
+    // const fetchServerData = async () => {
+    //   const url = `http://localhost:${__SERVER_PORT__}`
+    //   const response = await fetch(url)
+    //   const data = await response.json()
+    //   console.log(data)
+    // }
+    // fetchServerData()
   }, [])
 
   return (

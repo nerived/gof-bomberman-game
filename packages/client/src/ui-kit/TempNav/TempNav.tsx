2fe1d9947b749e7d16df24cb9476cd1fa4f237a4
eprikhodko <eprikhodkodev@gmail.com>
2024-03-05 22:29:11 +0100

    update nav

--- packages/client/src/ui-kit/TempNav/TempNav.tsx
+++ packages/client/src/ui-kit/TempNav/TempNav.tsx
@@ -7,12 +7,15 @@ import {
 import { Link } from 'react-router-dom'
 import { useAuth } from '../../features/user/hooks/useAuth'
 
+const Header = styled.header`
+  margin: 0 auto;
+`
+
 export const List = styled.ul`
   display: flex;
   gap: 40px;
   list-style: none;
   padding: 32px 16px 16px;
-  margin: 0 auto;
 `
 export const Item = styled.li`
   color: #fff;
@@ -31,15 +34,17 @@ export const TempNav = () => {
     : NotAuthorizedRoutesNav
 
   return (
-    <List>
-      {Object.keys(navRoutes).map(route => {
-        const href = RoutesPaths[route as keyof typeof RoutesPaths]
-        return (
-          <Item key={href}>
-            <StyledLink to={href}>{route}</StyledLink>
-          </Item>
-        )
-      })}
-    </List>
+    <Header>
+      <List>
+        {Object.keys(navRoutes).map(route => {
+          const href = RoutesPaths[route as keyof typeof RoutesPaths]
+          return (
+            <Item key={href}>
+              <StyledLink to={href}>{route}</StyledLink>
+            </Item>
+          )
+        })}
+      </List>
+    </Header>
   )
 }

8690cd03a4f3ffdbecda4c825cd5bd5cefed5930
Nikolay <n.smolov@yandex.ru>
2024-03-06 09:55:16 +0300

    GAME: adds api hooks

--- packages/client/src/pages/GamePage/GamePage.tsx
+++ packages/client/src/pages/GamePage/GamePage.tsx
@@ -1,7 +1,6 @@
 import { FC, useEffect } from 'react'
-import { GameComponent } from '../components/Game/GameComponent'
-import styled from 'styled-components'
-import { Colors } from '../tokens'
+import { GameComponent } from '../../components/Game/GameComponent'
+import { Main } from './GamePage.styled'
 
 const PAGE_ID = 'game-page'
 
@@ -25,15 +24,6 @@ function fullScreenOnFkey(e: KeyboardEvent) {
     toggleFullScreen()
   }
 }
-
-const Main = styled.main`
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  background-color: ${Colors.DEEP_DARK_BLUE};
-`
 
 export const GamePage: FC = () => {
   useEffect(() => {

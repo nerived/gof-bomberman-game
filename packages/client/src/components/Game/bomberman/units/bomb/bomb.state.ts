5e179a9507e16dab341e54ccbe107ce8a07c0541
nikolay <n.smolov@yandex.ru>
2024-03-07 02:44:29 +0300

    adds flame animation with sprites

--- packages/client/src/components/Game/bomberman/units/bomb/bomb.state.ts
+++ packages/client/src/components/Game/bomberman/units/bomb/bomb.state.ts
@@ -1,35 +1,21 @@
-import { BombUnit } from './bomb.unit'
-import bombImageSrc from '../../assets/bomb-sprite.png'
-import flameImageSrc from '../../assets/flame.png'
+import { BombUnit, SPRITE_INDEX, SPRITE_SIZE } from './bomb.unit'
 import explodeSoundSrc from '../../assets/explode.flac'
 
-export const SPRITE_SIZE = 112
-
-export const SPRITE_INDEX = {
-  IDLE: { posY: 0, frames: 8 },
-}
-
 export abstract class BombState {
-  constructor(protected bomb: BombUnit) {}
+  protected frame = 0
+  protected sprite = 0
+  constructor(protected readonly bomb: BombUnit) {}
 
   abstract draw(_canvasCtx: CanvasRenderingContext2D, _offsetX: number): void
 }
 
 class Idle extends BombState {
-  private frame = 0
-  private sprite = 0
-  protected _image
-  protected _bomb
   protected _timer = 3000
   protected _startTime
   protected _sound
 
   constructor(protected bomb: BombUnit) {
     super(bomb)
-
-    this._bomb = bomb
-    this._image = new Image()
-    this._image.src = bombImageSrc
     this._startTime = performance.now()
     this._sound = new Audio(explodeSoundSrc)
   }
@@ -44,67 +30,69 @@ class Idle extends BombState {
 
     if (
       performance.now() - this._startTime < this._timer &&
-      !this._bomb.startExplode
+      !this.bomb.startExplode
     ) {
       return
     }
 
     this._sound.play()
-    this._bomb.detonate()
-    this._bomb.changeState(new Explode(this._bomb))
+    this.bomb.detonate()
+    this.bomb.changeState(new Explode(this.bomb))
   }
 
   public draw(canvasCtx: CanvasRenderingContext2D, offsetX: number) {
     this._update()
 
     canvasCtx.drawImage(
-      this._image,
+      this.bomb.image,
       SPRITE_SIZE * this.sprite,
       SPRITE_SIZE * SPRITE_INDEX.IDLE.posY,
       SPRITE_SIZE,
       SPRITE_SIZE,
-      this._bomb.x + offsetX,
-      this._bomb.y,
-      this._bomb.width,
-      this._bomb.height
+      this.bomb.x + offsetX,
+      this.bomb.y,
+      this.bomb.width,
+      this.bomb.height
     )
   }
 }
 
 class Explode extends BombState {
-  protected _bomb
-  protected _image
   protected _explodeTime = 1000
-  protected _frame = -1
   protected _startTime
 
   constructor(protected bomb: BombUnit) {
     super(bomb)
-    this._bomb = bomb
-    this._image = new Image()
-    this._image.src = flameImageSrc
     this._startTime = performance.now()
   }
 
   private _update() {
-    this._frame++
+    this.frame++
 
-    if (this._frame === 4) {
-      this._bomb.exploded = true
-      this._bomb.command?.execute()
+    if (this.frame % 6 === 0) {
+      this.sprite = (this.sprite + 1) % SPRITE_INDEX.EXPLODE.frames
+    }
+
+    if (this.frame === 4 && !this.bomb.exploded) {
+      this.bomb.exploded = true
+      this.bomb.command?.execute()
     }
 
     if (performance.now() - this._startTime >= this._explodeTime) {
-      this._bomb.command?.execute()
+      this.bomb.command?.execute()
     }
   }
 
   public draw(canvasCtx: CanvasRenderingContext2D, offsetX: number) {
     this._update()
 
-    for (const drawable of [this._bomb, ...this._bomb.magnitude]) {
+    for (const drawable of [this.bomb, ...this.bomb.magnitude]) {
       canvasCtx.drawImage(
-        this._image,
+        this.bomb.image,
+        SPRITE_SIZE * this.sprite,
+        SPRITE_SIZE * SPRITE_INDEX.EXPLODE.posY,
+        SPRITE_SIZE,
+        SPRITE_SIZE,
         drawable.x + offsetX,
         drawable.y,
         drawable.width,
--- packages/client/src/components/Game/bomberman/units/bomb/bomb.unit.ts
+++ packages/client/src/components/Game/bomberman/units/bomb/bomb.unit.ts
@@ -1,8 +1,17 @@
 import { RectGameUnit, TGameUnit } from '../../basics/unit'
 import { ICommand } from '../../basics/command'
 import { STATE, BombState } from './bomb.state'
+import bombImageSrc from '../../assets/bomb-sprite.png'
+
+export const SPRITE_SIZE = 112
+
+export const SPRITE_INDEX = {
+  IDLE: { posY: 0, frames: 8 },
+  EXPLODE: { posY: 1, frames: 10 },
+}
 
 export class BombUnit extends RectGameUnit {
+  public image
   public passable = true
   public destroyable = true
   public readonly power
@@ -14,6 +23,9 @@ export class BombUnit extends RectGameUnit {
 
   constructor(power: number, x = 0, y = 0, width = 0, height = 0) {
     super(x, y, width, height)
+
+    this.image = new Image()
+    this.image.src = bombImageSrc
 
     this.power = power
     this.magnitude.push(this)
